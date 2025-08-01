149b077ee8fa1934df5ed1791a0e340c9d8c4d44
DheerajNarote <85816173+DheerajNarote@users.noreply.github.com>
2025-08-02 00:31:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var wt = document.getElementById("wt");
+var wt = document.getElementById("givenwt");
 var height = document.getElementById("height");
 var weight = document.getElementById("weight");
 // var male = document.getElementById("m");
 // var female = document.getElementById("f");
 var form = document.getElementById("form");
-let resultArea = document.querySelector(".comment");
+// let resultArea = document.querySelector(".comment");
 
 modalContent = document.querySelector(".modal-content");
 modalText = document.querySelector("#modalText");
@@ -27,44 +27,26 @@ function calculate() {
   } else {
     countBmi();
   }
-
 }
-
 
 function countBmi() {
   var p = [wt.value, height.value, weight.value];
-  // var p = [weight.value, dia.value, lenght.value];
-  // if(male.checked){
-  //   p.push("male");
-  // }else if(female.checked){
-  //   p.push("female");
-  // }
 
-  // var bmi = Number(p[2])/(Number(p[1])/100*Number(p[1])/100);
-  var bmi = Number(p[0] / (0.00616225 * p[1] * p[1] * p[2]) * 1000);
+  var bmi = Number(0.00616524 * p[1] * p[1] * p[2]);
 
+  var aa = Number(1 / (0.00616524 * p[1] * p[1] * p[2]) * 1000);
 
-  var result = '';
-  if (bmi < 18.5) {
-    result = 'Underweight';
-  } else if (18.5 <= bmi && bmi <= 24.9) {
-    result = 'Healthy';
-  } else if (25 <= bmi && bmi <= 29.9) {
-    result = 'Overweight';
-  } else if (30 <= bmi && bmi <= 34.9) {
-    result = 'Obese';
-  } else if (35 <= bmi) {
-    result = 'Extremely obese';
-  }
+  var cc = Number(450 / (0.00616524 * p[1] * p[1] * p[2]) * 1000);
 
+  var bb = Number(p[0] / (0.00616524 * p[1] * p[1] * p[2]) * 1000);
 
+document.querySelector("#result1").innerHTML = bmi.toFixed(4) + " Grams";
+document.querySelector("#result2").innerHTML = aa.toFixed(0) + " Nos";
+document.querySelector("#result4").innerHTML = cc.toFixed(0) + " Nos";
+document.querySelector("#result3").innerHTML = bb.toFixed(0) + " Nos";
 
-  resultArea.style.display = "block";
-  // document.querySelector(".comment").innerHTML = `You are <span id="comment">${result}</span>`;
-  document.querySelector("#result").innerHTML = bmi.toFixed(6);
-
+document.getElementById("gWt").innerHTML = wt.value;
 }
-
 
 
 
